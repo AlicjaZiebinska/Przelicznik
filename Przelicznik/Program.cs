8a8c53346dc0c9d56817d2993bf49e7c9e34b189
Alicjaz <alicjaziebinska@wp.pl>
2023-01-23 22:17:33 +0100

    Dodaj pliki projektów.

---
+++ Przelicznik/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przelicznik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w programie liczącym wynagrodzenie");
+            Umowa umowa = new Umowa();
+            Pracownik mojPracownik = umowa.daneUmowy();
+
+            Kalkulator kalkulator = new Kalkulator();
+            Console.WriteLine("Składka społeczna wynosi:" + kalkulator.spoleczna(mojPracownik));
+            Console.WriteLine("Składka zdrowotna wynosi:" + kalkulator.zdrowotna(mojPracownik));
+            if (mojPracownik.uopCzyZlecenie == "1")
+            {
+                Console.WriteLine("Podatek wynosi:" + (kalkulator.podatek(mojPracownik) - kalkulator.ulgaUoP(mojPracownik)));
+            }
+            else
+            {
+                Console.WriteLine("Podatek wynosi:" + (kalkulator.podatekZlecenie(mojPracownik) - kalkulator.ulgaZlecenie(mojPracownik)));
+            }
+            Console.WriteLine("Netto wynosi:" + kalkulator.obliczNetto(mojPracownik));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Przelicznik/Umowa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przelicznik
+{
+    internal class Umowa
+    {
+        public Pracownik daneUmowy()
+        {
+            Pracownik pracownik = new Pracownik();
+            Console.WriteLine("Podaj Nazwisko oraz Imię pracownika");
+            pracownik.nazwiskoImie = Console.ReadLine();
+            Console.WriteLine("Podaj wiek pracownika");
+            pracownik.wiek = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj kwotę brutto umowy");
+            pracownik.brutto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Jaki rodzaj umowy?");
+            Console.WriteLine("Umowa o pracę - wpisz 1");
+            Console.WriteLine("Umowa zlecenie - wpisz 2");
+            pracownik.uopCzyZlecenie = Console.ReadLine();
+            Console.WriteLine("Czy to jedyna umowa?");
+            Console.WriteLine("TAK - wpisz 1");
+            Console.WriteLine("NIE - wpisz 2");
+            pracownik.czyUlga = Console.ReadLine();
+            return pracownik;
+        }
+    }
+}
